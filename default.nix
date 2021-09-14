b209a6953309d56bee4b152f289947311d038489
Samuel Evans-Powell <mail@sevanspowell.net>
2021-09-14 10:16:25 +0800

    WIP

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 let
   nixpkgs = pkgs.lib.cleanSource pkgs.path;
 
-  vmConfig = configuration: import "${nixpkgs}/nixos/lib/eval-config.nix" {
+  vmConfig = configuration: (import "${nixpkgs}/nixos/lib/eval-config.nix" {
     inherit system ;
     modules = [
       configuration
@@ -14,6 +14,35 @@ let
         users.extraUsers.sam.initialPassword = "sam";
       }
     ];
-  };
+  });
+
+  isoConfig = (import "${nixpkgs}/nixos/lib/eval-config.nix" {
+    inherit system;
+    modules = [
+      "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+      "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+    ];
+  });
+
+  installerEnv = configuration:
+    let
+      evaluated = import "${nixpkgs}/nixos/lib/eval-config.nix" {
+        inherit system ;
+        modules = [ configuration ];
+      };
+
+      isoImage = isoConfig.config.system.build.isoImage;
+      isoName = isoConfig.config.isoImage.isoName;
+
+      systemName = evaluated.config.system.name;
+
+    in
+      pkgs.writeShellScriptBin "run-${systemName}-vm" ''
+        ${(vmConfig configuration).config.system.build.vm}/bin/run-${systemName}-vm -boot d -cdrom ${isoImage}/iso/${isoName}
+      '';
 in
-  (vmConfig ./nodes/plum/configuration.nix).config.system.build.vm
+{
+  vm = (vmConfig ./nodes/plum/configuration.nix).config.system.build.vm;
+  iso = isoConfig.config.system.build.isoImage;
+  install = installerEnv ./nodes/plum/configuration.nix;
+}
