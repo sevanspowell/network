1801fab63a3d9b338501962df98cfbeb4cc87995
Samuel Evans-Powell <mail@sevanspowell.net>
2022-07-23 12:38:40 +0800

    flake update

--- flake.nix
+++ flake.nix
@@ -6,10 +6,14 @@
     cardano-node.url = "github:input-output-hk/cardano-node?ref=refs/tags/1.33.0";
     deploy.url = "github:input-output-hk/deploy-rs";
     flake-utils.url = "github:numtide/flake-utils";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
     home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix.url = "github:NixOS/nix/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.follows = "haskellNix/nixpkgs-2111";
+    haskellNix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
   };
