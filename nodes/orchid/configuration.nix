33d340f33da6ee92d3ddf8b1f9c261d1598936c2
Samuel Evans-Powell <mail@sevanspowell.net>
2022-11-08 21:16:43 +0800

    Add substituters for reflex

--- nodes/orchid/configuration.nix
+++ nodes/orchid/configuration.nix
@@ -308,17 +308,16 @@ in
   users.extraGroups.vboxusers.members = [ "sam" ];
 
   nix.settings.substituters = [
-    "https://sevanspowell-personal.cachix.org"
     "https://cache.nixos.org"
     "https://iohk.cachix.org"
     "https://cache.iog.io"
+    "https://nixcache.reflex-frp.org"
   ];
   nix.settings.trusted-public-keys = [
-    "sevanspowell-personal.cachix.org-1:VOY8b19A+HGl1xUof+ucLFTDRCYBhjv+q94rxt5t5Bk="
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-    # "mantis-ops.cachix.org-1:SornDcX8/9rFrpTjU+mAAb26sF8mUpnxgXNjmKGcglQ="
+    "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
   ];
 
   nix.buildMachines = [
